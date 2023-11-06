e130faeb55ecc86b8e1b134184cd70ef7a8e2f47
cpalacior <cpalacior@eafit.edu.co>
2023-11-05 21:20:19 -0500

    creacion ruta de la imagen del pknea

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')
+var path = require('path');
+const Router  = require('./routes/routes');
+
 const app = express();
 const PORT = 3000;
-const Router  = require('./routes/routes');
+
+app.use(cors())
+
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
 
 app.use(Router);
-app.get('/', (req,res) => {
-    return res.send("hello World");
-})
 
 app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}!`));
